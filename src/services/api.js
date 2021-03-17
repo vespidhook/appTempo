d0d34cb5df7626b7f974e448357430231d9f1e4f
Bruno Alves <vespidhook@gmail.com>
2021-03-17 15:03:26 -0300

    up

---
+++ src/services/api.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+//https://api.hgbrasil.com/weather?key=db71b84b&lat=-23.682&lon=-46.875
+
+// export const key = '04dc0a88'
+ export const key = 'db71b84b';
+
+ const api = axios.create({
+     baseURL: 'https://api.hgbrasil.com'
+ });
+
+ export default api;
+
